73f29717de2315a521ca83c8ecc39b638e8a8610
Lee TB <tblyduc2412@gmail.com>
2023-05-28 12:11:15 +0700

    practical examples: conditionals and loops

---
+++ practical-examples/conditionals-and-loops/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+const show = ref(true);
+const list = ref([1, 2, 3]);
+</script>
+
+<template>
+  <button @click="show = !show">Toggle list</button>
+  <button @click="list.push(list.length + 1)">Push number</button>
+  <button @click="list.pop()">Pop number</button>
+  <button @click="list.reverse()">Reverse lise</button>
+
+  <ul v-if="list.length && show">
+    <li v-for="item in list">
+      {{ item }}
+    </li>
+  </ul>
+  <p v-else-if="list.length && !show">List is not empty, but hidden</p>
+  <p v-else>List is empty</p>
+</template>
+
+<style>
+button {
+  cursor: pointer;
+}
+</style>
