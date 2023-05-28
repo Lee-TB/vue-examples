a541e3649d6d7aec227b59e425feac5e2191a0e1
Lee TB <tblyduc2412@gmail.com>
2023-05-28 13:19:22 +0700

    practical examples: simple component

---
+++ practical-examples/simple-component/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+import TodoItem from './TodoItem.vue';
+
+const groceryList = ref([
+  { id: 0, text: 'Vegetables' },
+  { id: 1, text: 'Cheese' },
+  { id: 2, text: 'Whatever else humans are supposed to eat' },
+]);
+</script>
+
+<template>
+  <ol>
+    <TodoItem v-for="item of groceryList" :key="item.id" :todo="item" />
+  </ol>
+</template>
